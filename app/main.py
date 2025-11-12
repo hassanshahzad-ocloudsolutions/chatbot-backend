7db9c52b466c626b032c4d783d53a9c10d381073
Hassan Shahzad <hassan.shahzad@ocloudsolutions.net>
2025-11-12 15:03:33 +0500

    CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import user, chat, message
 from app.routers import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,3 +11,17 @@ Base.metadata.create_all(bind=engine)
 
 
 app.include_router(chat.router)
+
+
+# Allow frontend origin
+origins = [
+    "http://localhost:8080",  # frontend URL 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # allow your frontend origin(s)
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods
+    allow_headers=["*"],  # allow all headers (Authorization, Content-Type, etc.)
+)
