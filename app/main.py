a85f01039f68ffb04e14fb563690b88c00c3bc75
Hassan Shahzad <hassan.shahzad@ocloudsolutions.net>
2025-11-12 15:05:12 +0500

    CORS

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app.include_router(chat.router)
 
 # Allow frontend origin
 origins = [
-    "http://localhost:8080",  # frontend URL 
+    "http://localhost:8080",
+    "http://192.168.0.90:8080"
 ]
 
 app.add_middleware(
