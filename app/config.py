76f712783ff94586d4897ff4b4a095eb6e0f06ef
Hassan Shahzad <hassan.shahzad@ocloudsolutions.net>
2025-11-14 11:52:25 +0500

    config-change

--- app/config.py
+++ app/config.py
@@ -6,9 +6,17 @@ load_dotenv()
 
 # Database URL for PostgreSQL
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 # Firebase service account path
 FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
 
-# Any other API keys, tokens, or configs
-# Example: OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OLLAMA
+OLLAMA_API=os.getenv("OLLAMA_API_URL")
+OLLAMA_MODEL=os.getenv("OLLAMA_MODEL")
+OLLAMA_TEMPERATURE=os.getenv("OLLAMA_TEMPERATURE") 
+
+#OpenAI API
+OPENAI_API=os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL=os.getenv("OPENAI_MODEL")
+OPENAI_TEMPERATURE=os.getenv("OPENAI_TEMPERATURE")
+
+
--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from app.database import Base, engine
 from app.models import user, chat, message
 from app.routers import chat
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env
+load_dotenv()
+
 
 app = FastAPI()
 
@@ -15,9 +23,11 @@ app.include_router(chat.router)
 
 # Allow frontend origin
 origins = [
-    "http://localhost:8080", #local
-    "http://192.168.0.90:8080" #network
+    os.getenv("LOCALHOST_PATH1"), #local
+    os.getenv("LOCALHOST_PATH2") #network
 ]
+
+print(origins)
 
 app.add_middleware(
     CORSMiddleware,
